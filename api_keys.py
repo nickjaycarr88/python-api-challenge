7d3eb285ced2c6821574a837d0b79748a560a67a
“nickjaycarr88” <nickjaycarr88@gmail.com>
2022-08-05 14:41:23 +0800

    saving part one

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "89e35341d4f2f05cb8f128c6822e2481"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB_9NVENKerFZ5TeEGBiv52TgPiy0MXztc"
